ca24eb34ca15c02b3437b89157d68c9656970c8f
guywithhat99 <jami6753@colorado.edu>
2025-03-01 13:17:58 -0700

    Initial Commit

---
+++ CircuitDrawingApp/CircuitDrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitDrawingAppApp.swift
+//  CircuitDrawingApp
+//
+//  Created by Jack Miller on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CircuitDrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircuitDrawingAppTests/CircuitDrawingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitDrawingAppTests.swift
+//  CircuitDrawingAppTests
+//
+//  Created by Jack Miller on 3/1/25.
+//
+
+import Testing
+@testable import CircuitDrawingApp
+
+struct CircuitDrawingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
